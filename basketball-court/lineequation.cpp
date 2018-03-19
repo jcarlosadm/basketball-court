021b98ee73b53e66acbdfef4221113d5ba8d8a61
J Carlos Viana F <jcvf@ic.ufal.br>
2018-03-18 22:02:50 -0300

    fix lineequation

--- basketball-court/lineequation.cpp
+++ basketball-court/lineequation.cpp
@@ -10,12 +10,30 @@ void LineEquation::line(int x1, int y1, int x2, int y2, float color[3], Point pl
     int x, y;
     float a;
 
-    if (x2 != x1)
-        a = (y2 - y1) / (x2 - x1);
-    else
-        a = 0;
-    for(x = x1; x <= x2; ++x) {
-        y = (y1 + a * (x - x1));
-        plotter.draw(x, y, color);
+    if(x1 > x2){
+        int temp = x1;
+        x1 = x2;
+        x2 = temp;
     }
+
+    if (x2 != x1){
+        a = (y2 - y1) / (x2 - x1);
+
+        for(x = x1; x <= x2; ++x) {
+            y = (y1 + a * (x - x1));
+            plotter.draw(x, y, color);
+        }
+    }
+    else{
+        if(y1 > y2){
+            int temp = y1;
+            y1 = y2;
+            y2 = temp;
+        }
+
+        for(y = y1; y<=y2;++y){
+            plotter.draw(x1,y,color);
+        }
+    }
+
 }
