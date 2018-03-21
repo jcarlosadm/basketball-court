5d47ebceab92ca4d027654174d6d71ef7299fcc4
J Carlos Viana F <jcvf@ic.ufal.br>
2018-03-21 11:53:07 -0300

    this Closes #2. better basketball court

--- basketball-court/circlebresenham.cpp
+++ basketball-court/circlebresenham.cpp
@@ -1,25 +1,28 @@
 #include "circlebresenham.h"
 
-void drawCircle(int centerx, int centery, int x, int y, float color[], Point plotter)
+void drawCircle(int centerx, int centery, int x, int y, float color[], Point plotter, bool left, bool right)
 {
-    plotter.draw(centerx + x, centery + y, color);
-    plotter.draw(centerx - x, centery + y, color);
-    plotter.draw(centerx + x, centery - y, color);
-    plotter.draw(centerx - x, centery - y, color);
-    plotter.draw(centerx + y, centery + x, color);
-    plotter.draw(centerx - y, centery + x, color);
-    plotter.draw(centerx + y, centery - x, color);
-    plotter.draw(centerx - y, centery - x, color);
+    if (right) {
+        plotter.draw(centerx + x, centery + y, color);
+        plotter.draw(centerx + x, centery - y, color);
+        plotter.draw(centerx + y, centery + x, color);
+        plotter.draw(centerx + y, centery - x, color);
+    }
+
+    if (left) {
+        plotter.draw(centerx - x, centery + y, color);
+        plotter.draw(centerx - x, centery - y, color);
+        plotter.draw(centerx - y, centery + x, color);
+        plotter.draw(centerx - y, centery - x, color);
+    }
 }
 
-// TODO: implement
-void CircleBresenham::circle(int centerx, int centery, int radius, float color[], Point plotter)
-{
+void draw_(int centerx, int centery, int radius, float color[], Point plotter, bool left, bool right) {
     int x = 0, y = radius;
     int d = 3 - 2 * radius;
     while (y >= x)
     {
-        drawCircle(centerx, centery, x, y, color, plotter);
+        drawCircle(centerx, centery, x, y, color, plotter, left, right);
         ++x;
 
         if (d > 0)
@@ -29,6 +32,21 @@ void CircleBresenham::circle(int centerx, int centery, int radius, float color[]
         }
         else
             d = d + 4 * x + 6;
-        drawCircle(centerx, centery, x, y, color, plotter);
+        drawCircle(centerx, centery, x, y, color, plotter, left, right);
     }
+}
+
+void CircleBresenham::circle(int centerx, int centery, int radius, float color[], Point plotter)
+{
+    draw_(centerx, centery, radius, color, plotter, true, true);
+}
+
+void CircleBresenham::semicircle_left(int centerx, int centery, int radius, float color[], Point plotter)
+{
+    draw_(centerx, centery, radius, color, plotter, true, false);
+}
+
+void CircleBresenham::semicircle_right(int centerx, int centery, int radius, float color[], Point plotter)
+{
+    draw_(centerx, centery, radius, color, plotter, false, true);
 }
--- basketball-court/field.cpp
+++ basketball-court/field.cpp
@@ -7,19 +7,38 @@ void Field::draw(CircleAlgorithm *circlealg, LineAlgorithm *linealg, int centerx
     int width = 1000;
     int height = 500;
 
+    // setup
     Rectangle entireField((int)-width/2.0,(int) height/2.0, width, height);
-    Rectangle leftSquare((int)-width/2.0,(int) height/4.0,(int) width/5.0,(int) height/2.0);
-    leftSquare.adjustright(0.7f);
-    Rectangle rightSquare((int)width/2.0 - width/5.0,(int) height/4.0,(int) width/5.0,(int) height/2.0);
-    rightSquare.adjustleft(0.7f);
+    Rectangle leftSquare((int)-width/2.0,(int) height/6.0 - 23,(int) width/5.0,(int) height/4.0 - 5);
+    Rectangle rightSquare((int)width/2.0 - width/5.0,(int) height/6.0 - 23,(int) width/5.0,(int) height/4.0 - 5);
 
-    circlealg->circle(centerx,centery, 80, color, plotter);
+    // draw
+    circlealg->circle(centerx,centery, 100, color, plotter);
+    circlealg->circle(centerx,centery, 20, color, plotter);
 
     entireField.draw(linealg,color,plotter);
     leftSquare.draw(linealg,color,plotter);
     rightSquare.draw(linealg,color,plotter);
     linealg->line(centerx, centery - (int) height/2.0, centerx, centery + (int) height/2.0,color, plotter);
 
-    circlealg->circle(centerx-(int)width/2.5,centery, 40, color, plotter);
-    circlealg->circle(centerx+(int)width/2.5,centery, 40, color, plotter);
+    circlealg->circle((int)-width/2.0 + (int) width/5.0,centery, 50, color, plotter);
+    circlealg->circle((int) width/2.0 - (int) width/5.0,centery, 50, color, plotter);
+
+    circlealg->circle((int)-width/2.0 + 45,centery, 10, color, plotter);
+    circlealg->circle((int) width/2.0 - 45,centery, 10, color, plotter);
+
+    linealg->line((int) - width/2.0 + 35, centery + 20, (int) -width/2.0 + 35, centery - 20, color, plotter);
+    linealg->line((int) width/2.0 - 35, centery + 20, (int) width/2.0 - 35, centery - 20, color, plotter);
+
+    linealg->line((int) - width/2.0, centery + (int) height/3.0, (int) -width/2.0 + (int) width/7.0,
+                  centery + (int) height/3.0, color, plotter);
+    linealg->line((int) - width/2.0, centery - (int) height/3.0, (int) -width/2.0 + (int) width/7.0,
+                  centery - (int) height/3.0, color, plotter);
+    linealg->line((int) width/2.0, centery + (int) height/3.0, (int) width/2.0 - (int) width/7.0,
+                  centery + (int) height/3.0, color, plotter);
+    linealg->line((int) width/2.0, centery - (int) height/3.0, (int) width/2.0 - (int) width/7.0,
+                  centery - (int) height/3.0, color, plotter);
+
+    circlealg->semicircle_right((int) -width/2.0 + (int) width/7.0, centery, (int) height/3.0, color, plotter);
+    circlealg->semicircle_left((int) width/2.0 - (int) width/7.0, centery, (int) height/3.0, color, plotter);
 }
